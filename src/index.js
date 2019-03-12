8462765238974bb9c749de169378c11b1aeeab1d
Pavel Khareuka <paul.horevko@gmail.com>
2019-03-12 13:03:27 +0300

    add decision

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  let value = currency;
+  let result = {};
+  
+  let money = {
+    'H': 50,
+    'Q': 25,
+    'D': 10,
+    'N': 5,
+    'P': 1
+  };
+
+  for (let key in money) {
+    if (value >= money[key]) {
+      result[key] = Math.floor( value / money[key] );
+      value = value - result[key] * money[key];
+    }
+    if (value == 0) return result;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
